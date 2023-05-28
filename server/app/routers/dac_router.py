9f152a1b5f644c82e8a2f43670d68a2a4b851fab
Petr Janik <peta.janik@email.cz>
2023-05-28 16:52:13 +0200

    Reformat

--- server/app/routers/dac_router.py
+++ server/app/routers/dac_router.py
@@ -18,14 +18,18 @@ class DacSolution(TypedDict):
 @router.get("/")
 def get_deployed_dac_solutions() -> list[DacSolution]:
     mock_data: list[DacSolution] = [
-        {"id": "1",
-         "customerName": "Car factory",
-         "customerEmail": "jbpm.pv207@gmail.com",
-         "metricsUrl": "http://127.0.0.1:8000/dac/1/metrics"},
-        {"id": "2",
-         "customerName": "Conscious village",
-         "customerEmail": "jbpm.pv207@gmail.com",
-         "metricsUrl": "http://127.0.0.1:8000/dac/2/metrics"},
+        {
+            "id": "1",
+            "customerName": "Car factory",
+            "customerEmail": "jbpm.pv207@gmail.com",
+            "metricsUrl": "http://127.0.0.1:8000/dac/1/metrics",
+        },
+        {
+            "id": "2",
+            "customerName": "Conscious village",
+            "customerEmail": "jbpm.pv207@gmail.com",
+            "metricsUrl": "http://127.0.0.1:8000/dac/2/metrics",
+        },
     ]
 
     return mock_data
@@ -49,7 +53,7 @@ def get_dac_metrics(dac_id: str) -> DacMetrics:
             "cpuUsage": 90,
             "temperature": 50,
             "backupBatteryLife": 95,
-        }
+        },
     }
 
     dac_metric = mock_data.get(dac_id)

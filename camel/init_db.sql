c283b0b8399389376e12b9e9812e912d9295f0c7
Petr Janik <peta.janik@email.cz>
2023-05-28 16:50:51 +0200

    Complete Camel endpoints for audit process

--- camel/init_db.sql
+++ camel/init_db.sql
@@ -1,13 +1,18 @@
+DROP TABLE IF EXISTS processes_needing_adjustment;
+DROP TABLE IF EXISTS delayed_findings;
+
 CREATE TABLE processes_needing_adjustment
 (
     id          INTEGER PRIMARY KEY,
     processName TEXT NOT NULL,
+    description TEXT NOT NULL,
     date        TEXT NOT NULL
 );
 
 CREATE TABLE delayed_findings
 (
     id          INTEGER PRIMARY KEY,
-    findingName TEXT NOT NULL,
+    processName TEXT NOT NULL,
+    description TEXT NOT NULL,
     date        TEXT NOT NULL
 );
